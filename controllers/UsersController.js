e80850b469528e4e2a454ff8354a420119b8aa59
Ahmed <ahmed_l2@yahoo.com>
2024-08-31 21:19:46 +0300

    3. Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,7 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-class UsersController {
+export default class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
@@ -10,17 +10,18 @@ class UsersController {
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
+    try {
+      const existingUser = await dbClient.client.db().collection('users').findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
 
-    const existingUser = await dbClient.client.db().collection('users').findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
+      const hashedPassword = sha1(password);
+      const result = await dbClient.client.db().collection('users').insertOne({ email, password: hashedPassword });
+
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
     }
-
-    const hashedPassword = sha1(password);
-    const result = await dbClient.client.db().collection('users').insertOne({ email, password: hashedPassword });
-
-    return res.status(201).json({ id: result.insertedId, email });
   }
 }
-
-export default UsersController;
