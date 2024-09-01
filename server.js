256b40a1dbeadb0043653ac6bef923f0b1497c1a
Ahmed <ahmed_l2@yahoo.com>
2024-09-02 02:25:52 +0300

    fixed potientiall issue

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json());
 
-app.use('/', router);
+app.use(express.json({ limit: '50mb' }));
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
