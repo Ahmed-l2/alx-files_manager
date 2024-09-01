af50199b68ffa5fe8cbd5793d178a1a65a0573c0
Ahmed <ahmed_l2@yahoo.com>
2024-09-01 21:59:26 +0300

    4. Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -20,6 +22,23 @@ class UsersController {
       return res.status(201).json({ id: result.insertedId, email });
     } catch (err) {
       console.error('Error in postNew:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getMe(req, res) {
+    try {
+      const token = req.headers['x-token'];
+      if (!token) return res.status(401).json({ error: 'Unauthorized' });
+      const key = `auth_${token}`;
+      const userId = await redisClient.get(key);
+      if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+      const usersCollection = await dbClient.usersCollection();
+      const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+      if (!user) return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(200).json({ id: user._id, email: user.email });
+    } catch (err) {
+      console.error('Error in getMe:', err);
       return res.status(500).json({ error: 'Internal server error' });
     }
   }
