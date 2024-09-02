bb623b86a29f75dc40727fb8c4de455a3bc430b8
Ahmed <ahmed_l2@yahoo.com>
2024-09-03 00:23:36 +0300

    .

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -77,7 +77,14 @@ class FilesController {
     const filesCollection = await dbClient.filesCollection();
     const file = await filesCollection.findOne({ _id: ObjectId(req.params.id), userId });
     if (!file) return res.status(404).json({ error: 'Not found' });
-    return res.status(200).json(file);
+    return res.status(200).send({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
   }
 
   static async getIndex(req, res) {
