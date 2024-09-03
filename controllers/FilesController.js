6ffa87857b39bee04ef3df95256b999b4657a3cb
Ahmed <ahmed_l2@yahoo.com>
2024-09-03 04:14:15 +0300

    Fixed Lint

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -20,6 +20,7 @@ class FilesController {
     }
     return null;
   }
+
   static async postUpload(req, res) {
     const token = req.headers['x-token'];
     if (!token) return res.status(401).json({ error: 'Unauthorized' });
